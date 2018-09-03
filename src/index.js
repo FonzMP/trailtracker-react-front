41712adca76782c51300991a5d93824dcfadcc2f
fonzmp <fonzmp@gmail.com[color]>
2018-09-03 00:02:58 -0400

    added packages and items for router and middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import TrailsContainer from "./containers/TrailsContainer";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import manageTrails from "./reducers/trails_reducer";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(manageTrails, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <React.Fragment>
+        <Route exact path="/" component={App} />
+        <Route exact path="/trails" component={TrailsContainer} />
+      </React.Fragment>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
