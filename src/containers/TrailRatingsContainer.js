699b6c7b1b36bb00401cd720ac5eaf381c3a1edf
Alex Wilson <fonzmp@gmail.com>
2018-09-05 13:49:05 -0400

    git conflict corrected

--- src/containers/TrailRatingsContainer.js
+++ src/containers/TrailRatingsContainer.js
@@ -5,7 +5,6 @@ import Navigation from './Navigation'
 import { connect } from "react-redux";
 import { fetchRatings, addTrailRating, deleteTrailRating } from "../actions/trailRatingsActions";
 import { fetchTrails } from "../actions/trailActions";
-import { addTrailRating } from "../actions/trailRatingsActions"
 
 class TrailRatingsContainer extends Component {
   componentDidMount() {
