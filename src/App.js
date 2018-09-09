39a400ca0f189defc50a85fc098ab0ab29bfcf2e
Alex Wilson <fonzmp@gmail.com>
2018-09-08 23:42:56 -0400

    added class content-body to limit width

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>
-          TrailTracker is an application designed to provide ease of mind for
-          the solo, and/or safety concious rider in all of us. Our focus is to
-          provide an application that assists in keeping our users safe while
-          adventuring alone, in small groups, or with friends in the great
-          outdoors.
-        </p>
+        <div className="content-body">
+          <p>
+            TrailTracker is an application designed to provide ease of mind for
+            the solo, and/or safety concious rider in all of us. Our focus is to
+            provide an application that assists in keeping our users safe while
+            adventuring alone, in small groups, or with friends in the great
+            outdoors.
+          </p>
+        </div>
       </div>
     );
   }
