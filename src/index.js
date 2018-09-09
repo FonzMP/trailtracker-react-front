6d22bc865d5d7d3ea819b8128198563b14763c34
fonzmp <fonzmp@gmail.com[color]>
2018-09-09 18:17:18 -0400

    organized imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
 import "./containers/navigation.css";
 import "./containers/style.css";
@@ -7,11 +10,8 @@ import App from "./App";
 import TrailsContainer from "./containers/TrailsContainer";
 import TrailRatingsContainer from "./containers/TrailRatingsContainer";
 import rootReducer from "./reducers/rootReducer";
-import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./containers/Navigation";
 
-import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
