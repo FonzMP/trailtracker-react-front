28c91b10681a0fe6f6633662f7f5ff92cc8aafe3
Alex Wilson <fonzmp@gmail.com>
2018-09-08 23:46:31 -0400

    removed previous class code

--- src/components/trail_ratings/ShowTrailRating.js
+++ src/components/trail_ratings/ShowTrailRating.js
@@ -21,28 +21,4 @@ const ShowTrailRating = ({ rating, edit, delete: deleteAction }) => {
   );
 };
 
-// class ShowTrailRating extends Component {
-
-//   render() {
-//     return (
-//       <div className="trail">
-//           <p>
-//             <strong>Name: </strong>
-//             <span>{this.props.rating.trail.name}</span>
-//           </p>
-//           <p>
-//             <strong>Length: </strong>
-//             <span>{this.props.rating.trail.length}</span>
-//           </p>
-//           <p>
-//             <strong>Rating: </strong>
-//             <span>{this.props.rating.rating}</span>
-//           </p>
-//           <button onClick={() => this.props.edit()}>Edit Rating</button>
-//           <button onClick={() => this.props.delete(this.props.rating.id)}>Delete Rating</button>
-//       </div>
-//     )
-//   }
-// }
-
 export default ShowTrailRating;
