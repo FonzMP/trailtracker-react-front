d5280def85fa61a42369cadde779ef2b7ff56135
Alex Wilson <fonzmp@gmail.com>
2018-09-08 23:49:09 -0400

    converted to presentational component

--- src/components/trails/ShowTrail.js
+++ src/components/trails/ShowTrail.js
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ShowTrail extends Component {
-  render() {
-    return (
-      <div className="trail">
-        <p>
-          <strong>Name: </strong>
-          <span>{this.props.trail.name}</span>
-        </p>
-        <p>
-          <strong>Length: </strong>
-          <span>{this.props.trail.length}</span>
-        </p>
-        <p>
-          <strong>Average Rating: </strong>
-          <span>
-            {Number(this.props.trail.average_rating) ===
-              this.props.trail.average_rating &&
-            this.props.trail.average_rating % 1 !== 0
-              ? this.props.trail.average_rating.toFixed(1)
-              : this.props.trail.average_rating}
-          </span>
-        </p>
-        <button onClick={() => this.props.edit()}>Edit Trail</button>
-        <button onClick={() => this.props.delete(this.props.trail.id)}>
-          Delete Trail
-        </button>
-      </div>
-    );
-  }
+const ShowTrail = ({trail, edit, delete: deleteAction }) => {
+  return (
+    <div className="trail">
+      <p>
+        <strong>Name: </strong>
+        <span>{trail.name}</span>
+      </p>
+      <p>
+        <strong>Length: </strong>
+        <span>{trail.length}</span>
+      </p>
+      <p>
+        <strong>Average Rating: </strong>
+        <span>
+          {Number(trail.average_rating) ===
+            trail.average_rating &&
+          trail.average_rating % 1 !== 0
+            ? trail.average_rating.toFixed(1)
+            : trail.average_rating}
+        </span>
+      </p>
+      <button onClick={() => edit()}>Edit Trail</button>
+      <button onClick={() => deleteAction(trail.id)}>
+        Delete Trail
+      </button>
+    </div>
+  );
 }
 
 export default ShowTrail;
